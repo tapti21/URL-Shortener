e69e3beed301441b1591130b10094b3198a337e7
Trapti <122607488+tapti21@users.noreply.github.com>
2024-02-12 12:12:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express= require('express');
+const app= express();
+const urlRoute = require('./routes/router');
+port=3000;
+const path= require('path')
+
+app.use(express.urlencoded({ extended:false }))
+app.use(express.json())
+
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
+
+app.use('/url',urlRoute)
+
+app.use('/url/analytics',urlRoute)
+
+app.listen(port,()=>{
+    console.log(`server running ${port}`);
+})
